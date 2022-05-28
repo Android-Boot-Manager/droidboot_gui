9dabfa7e79f47ee1596bfa68c05bf7af1ca1505a
luka177 <lukapanio@gmail.com>
2022-05-28 14:09:56 +0300

    Add libs

---
+++ lib/lib.mk
@@ -0,0 +1,4 @@
+LOCAL_PATH := $(shell pwd)
+
+include $(LOCAL_PATH)/ext4/ext4.mk
+include $(LOCAL_PATH)/lvgl/lvgl.mk
--- lk.mk
+++ lk.mk
@@ -3,3 +3,4 @@ LOCAL_PATH := $(shell pwd)
 include $(LOCAL_PATH)/dualboot_gui/dualboot_gui.mk
 include $(LOCAL_PATH)/config_parser/config_parser.mk
 include $(LOCAL_PATH)/backend/backend.mk
+include $(LOCAL_PATH)/lib/lib.mk
