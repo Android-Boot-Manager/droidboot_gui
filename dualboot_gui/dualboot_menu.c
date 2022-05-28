60042b8b7f50df8fbe155bae9269034c26aaa763
luka177 <lukapanio@gmail.com>
2022-05-28 13:33:22 +0300

    Initial file structure

---
+++ dualboot_gui/dualboot_menu.c
@@ -0,0 +1,5 @@
+// This file contains a high level, universal (not platform dependent) code for displaying ABM gui menu
+// At this stage we except that display is inited and lower level code is already executed.
+#include <lvgl.h>
+// droidboot_platforms_common.h is part of droidboot_platform and contains all functions used by droidboot gui
+#include <droidboot_platforms_common.h>
---
+++ lk.mk
@@ -0,0 +1,5 @@
+LOCAL_PATH := $(shell pwd)
+
+include $(LOCAL_PATH)/dualboot_gui/dualboot_gui.mk
+include $(LOCAL_PATH)/config_parser/config_parser.mk
+include $(LOCAL_PATH)/backend/backend.mk
