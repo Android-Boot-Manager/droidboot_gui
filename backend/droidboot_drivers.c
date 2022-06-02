163c06d7ff7f2a8e99442f0accb8e2be057ef532
Dom Rodriguez <shymega@shymega.org.uk>
2022-06-02 16:39:39 +0100

    droidboot_drivers: Tidy up syntax and cosmetics a little bit
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- backend/droidboot_drivers.c
+++ backend/droidboot_drivers.c
@@ -3,11 +3,11 @@
 #include <droidboot_platforms/common/droidboot_platform_common.h>
 
 droidboot_ret droidboot_driver_init(){
-    droidboot_ret ret=DROIDBOOT_EOK;
+    droidboot_ret ret = DROIDBOOT_EOK;
     
     // Run platform init
     ret = droidboot_platform_init();
-    if(ret!=DROIDBOOT_EOK)
+    if (ret!=DROIDBOOT_EOK)
         return ret;
      
     // Init lvgl   
@@ -17,23 +17,24 @@ droidboot_ret droidboot_driver_init(){
     static lv_color_t buf_1[droidboot_platfoems_get_hor_res() * 500];
     static lv_color_t buf_2[droidboot_platfoems_get_hor_res() * 500];
 
-	lv_disp_draw_buf_init(&disp_buf, buf_1, buf_2, droidboot_platfoems_get_hor_res()*500);
-	static lv_disp_drv_t disp_drv;          /*A variable to hold the drivers. Must be static or global.*/
-    lv_disp_drv_init(&disp_drv);            /*Basic initialization*/
-	disp_drv.draw_buf = &disp_buf;          /*Set an initialized buffer*/
-	disp_drv.flush_cb = droidboot_fb_flush;        /*Set a flush callback to draw to the display*/
-	disp_drv.hor_res = droidboot_platfoems_get_hor_res();                 /*Set the horizontal resolution in pixels*/
-	disp_drv.ver_res = droidboot_platfoems_get_ver_res();                 /*Set the vertical resolution in pixels*/
+	lv_disp_draw_buf_init(&disp_buf, buf_1, buf_2, droidboot_platfoems_get_hor_res() * 500);
+	static lv_disp_drv_t disp_drv;          /* A variable to hold the drivers. Must be static or global. */
+    lv_disp_drv_init(&disp_drv);            /* Basic initialization */
+	disp_drv.draw_buf = &disp_buf;          /* Set an initialized buffer */
+	disp_drv.flush_cb = droidboot_fb_flush;        /* Set a flush callback to draw to the display */
+	disp_drv.hor_res = droidboot_platfoems_get_hor_res();                 /* Set the horizontal resolution in pixels  */
+	disp_drv.ver_res = droidboot_platfoems_get_ver_res();                 /* Set the vertical resolution in pixels */
 
 	lv_disp_t * disp;
-	disp = lv_disp_drv_register(&disp_drv); /*Register the driver and save the created display objects*/
+	disp = lv_disp_drv_register(&disp_drv); /* Register the driver and save the created display objects */
 	
 	// Set up keys
-	lv_indev_drv_init(&indev_drv);      /*Basic initialization*/
+	lv_indev_drv_init(&indev_drv);      /* Basic initialization */
     indev_drv.type = LV_INDEV_TYPE_KEYPAD;
     indev_drv.read_cb = droidboot_key_read;
-    /*Register the driver in LVGL and save the created input device object*/
-    lvgl_mtk_indev = lv_indev_drv_register(&indev_drv);
     
+    /* Register the driver in LVGL and save the created input device object */
+    lvgl_mtk_indev = lv_indev_drv_register(&indev_drv)
+
     return ret;
 }
