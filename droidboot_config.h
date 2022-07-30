c48ea52da5f9faceb3c84965264e27cdec251e73
Dom Rodriguez <shymega@shymega.org.uk>
2022-07-30 19:44:36 +0100

    Fix typo on `DROIDBOOT_NO_LOGGING` coment
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- droidboot_config.h
+++ droidboot_config.h
@@ -4,7 +4,7 @@
 // DROIDBOOT_SYSTEM_LOG can be 1 or 0, enables logging with printf wich is usually handled at platform side. Recomended.
 #define DROIDBOOT_SYSTEM_LOG 1
 
-// DROIDBOOT_NO_LOGGING can be 1 or 0, overwrites previous 2 and disables any logging from droidboot. Must be treated with care, and tottaly not reccomended.
+// DROIDBOOT_NO_LOGGING can be 1 or 0, overrides previous 2 defines and disables any logging from droidboot. Must be treated with care, and totally not recommended.
 //#define DROIDBOOT_NO_LOGGING 0
 
 // DROIDBOOT_LOG_LEVEL can be 0-5 where 0 - trace, 1 - info, 2 - warning, 3 - error, 4 - fail, 5 - nothing
