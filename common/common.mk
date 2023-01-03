bfc53cdeafc21cd959ac677194ee451def6cc7aa
luka177 <lukapanio@gmail.com>
2023-01-03 11:24:53 +0100

    Fix lk compilation

--- common/common.mk
+++ common/common.mk
@@ -7,4 +7,5 @@ OBJS += \
 	$(LOCAL_DIR)/droidboot_logging.o \
 	$(LOCAL_DIR)/droidboot_main.o \
 	$(LOCAL_DIR)/droidboot_screens.o \
-	$(LOCAL_DIR)/droidboot_helpers.o
+	$(LOCAL_DIR)/droidboot_helpers.o \
+	$(LOCAL_DIR)/droidboot_stdfunc.o
